f8ffc0543c17fdeb859265401346240f1ce73431
Ilhom Abdulahatov <abdulahatov03gmail.com>
2022-12-27 04:34:33 +0300

    for loop and switch case

---
+++ app.js
@@ -0,0 +1,54 @@
+// switch case 
+// let data = 'qr'
+
+// switch (data) {
+//     case 'yomgir':
+//         console.log('yomgir');
+//         break;
+//     case 'qor':
+//         console.log('qor');
+//         break
+//     default:
+//         console.log('nomalum ob havo');
+//         break;
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i);
+//     }
+
+// }
+
+// let browser='Operas';
+
+
+// 1
+// switch (browser) {
+//     case 'Edge':
+//         alert(`You've got the Edge`)
+//         break;
+//     case 'Chrome':
+//     case 'Firefox':
+//     case 'Safari':
+//     case 'Opera':
+//         alert(`Okay we support these browser too`);
+//         break;
+//     default:
+//         alert(`We hope that this page looks ok!`)
+// }
+
+// 2
+// let a = +prompt('a?', '');
+
+// if (a == 0) {
+//     alert(0);
+// }
+// if (a == 1) {
+//     alert(1);
+// }
+
+// if (a == 2 || a == 3) {
+//     alert('2,3')
+// }
+
